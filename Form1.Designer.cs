e3cb6df1aa33a68daf5326e47eb09bdd6c906297
Alam <mczynko@hotmail.com>
2025-12-04 22:04:33 -0600

    Agregar TextBox y TableLayout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            txtDisplay = new TextBox();
+            tblBotones = new TableLayoutPanel();
+            SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            txtDisplay.Dock = DockStyle.Top;
+            txtDisplay.Font = new Font("Segoe UI", 20.1428585F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtDisplay.Location = new Point(0, 0);
+            txtDisplay.Multiline = true;
+            txtDisplay.Name = "txtDisplay";
+            txtDisplay.ReadOnly = true;
+            txtDisplay.Size = new Size(690, 168);
+            txtDisplay.TabIndex = 0;
+            txtDisplay.Text = "0";
+            txtDisplay.TextAlign = HorizontalAlignment.Right;
+            // 
+            // tblBotones
+            // 
+            tblBotones.ColumnCount = 4;
+            tblBotones.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tblBotones.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tblBotones.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tblBotones.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tblBotones.Dock = DockStyle.Fill;
+            tblBotones.Location = new Point(0, 168);
+            tblBotones.Name = "tblBotones";
+            tblBotones.RowCount = 5;
+            tblBotones.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tblBotones.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tblBotones.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tblBotones.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tblBotones.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tblBotones.Size = new Size(690, 645);
+            tblBotones.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(19F, 47F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(690, 813);
+            Controls.Add(tblBotones);
+            Controls.Add(txtDisplay);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox txtDisplay;
+        private TableLayoutPanel tblBotones;
     }
 }
